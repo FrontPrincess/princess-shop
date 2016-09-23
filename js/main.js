fc777decc6cdb8c323231b6302e483f29a2cb690
unknown <natalishapov@live.com>
2016-09-23 20:38:27 +0300

    add elavatezoom

--- js/main.js
+++ js/main.js
@@ -5,6 +5,23 @@ $(document).ready(function() {
         selectSmartPositioning: false
     });
 
+    //initiate the plugin and pass the id of the div containing gallery images
+    $("#zoom_03").elevateZoom({
+        gallery: 'gallery_01',
+        cursor: 'pointer',
+        galleryActiveClass: 'active',
+        imageCrossfade: true,
+        responsive: true,
+        /*loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'
+         */
+    });
+
+    //pass the images to Fancybox
+    $("#zoom_03").bind("click", function(e) {
+        var ez = $('#zoom_03').data('elevateZoom');
+        $.fancybox(ez.getGalleryList());
+        return false;
+    });
 
 
 });
